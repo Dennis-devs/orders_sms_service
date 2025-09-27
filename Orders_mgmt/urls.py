224ad7e64c44bae00b394ee1e0afb86753e152bc
Dennis <celestineakoth@Michaels-MacBook-Pro-2.local>
2025-09-28 02:29:02 +0300

    feat: completed customer sms alerting service

--- Orders_mgmt/urls.py
+++ Orders_mgmt/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+# from .views import SendConfirmationSMSView
 from .views import OrderViewSet, CustomerViewSet
+
 
 router = DefaultRouter()
 router.register(r'customers', CustomerViewSet)
 router.register(r'orders', OrderViewSet)
-# router.register(r'index', IndexViewSet, basename='index')
+
 urlpatterns = [
     path('', include(router.urls)), 
+    # path('send-confirmation/', SendConfirmationSMSView.as_view(), name='send-confirmation')
 ]
     
