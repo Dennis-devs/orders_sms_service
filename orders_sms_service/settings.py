8bd379aec6c809437c041f08ad0e4d3cee735dc1
Dennis <celestineakoth@Michaels-MacBook-Pro-2.local>
2025-10-07 00:01:05 +0300

    Added CI/CD workflow for Google App Engine

--- orders_sms_service/settings.py
+++ orders_sms_service/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
+
 # Load environment variables
 load_dotenv()
 
@@ -25,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-36-wzp#a)is(z^8igjrx+trfxzyrzgnqz%)zx6wtdz!d^17sk#'
-
+SEC = os.getenv('SEC')
+SECRET_KEY =os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
