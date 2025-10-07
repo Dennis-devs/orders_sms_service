c5c3ea1807e51d566d98e41c4a02693d3c709254
Dennis <celestineakoth@Michaels-MacBook-Pro-2.local>
2025-10-07 15:02:59 +0300

    Configured Django for Google App Engine

--- orders_sms_service/settings.py
+++ orders_sms_service/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
-
+import dj_database_url
 
 # Load environment variables
 load_dotenv()
@@ -26,8 +26,24 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SEC = os.getenv('SEC')
-SECRET_KEY =os.getenv('SECRET_KEY')
+# SEC = os.getenv('SEC')
+SECRET_KEY =os.getenv('SECRET_KEY', 'SEC' )
+DEBUG = os.getenv('DJANGO_ENV', 'development') == 'development'
+ALLOWED_HOSTS = [
+    'your-project-id-.ew.r.appspot.com',
+    'localhost',
+    '123.0.0.1'
+]
+
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_REFERRER_POLICY = 'strict-origin-when-cross-origin'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -80,6 +96,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_THROTTLE_CLASSES': ['rest_framework.throttling.UserRateThrottle'],
+    'DEFAULT_THROTTLE_RATES': {'user': '100/hour'}
 }
 
 MIDDLEWARE = [
@@ -117,12 +135,29 @@ WSGI_APPLICATION = 'orders_sms_service.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
+DATABASES2 = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600,
+        ssl_require=not DEBUG
+    )
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {'console': {'class': 'logging.StreamHandler'}},
+    'loggers': {'': {'handlers': ['console'], 'level': 'INFO'}},
+}
+
+
 
 
 # Password validation
@@ -163,6 +198,8 @@ STATIC_URL = '/static/'
 STATIC_DIRS = [
     BASE_DIR / 'static'
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
