7fb7bb22d1ebfbca009303f365cf554901213baf
Dennis <celestineakoth@Michaels-MacBook-Pro-2.local>
2025-10-15 01:00:38 +0300

    turned debug to false for production

--- orders_sms_service/settings.py
+++ orders_sms_service/settings.py
@@ -44,7 +44,7 @@ if not DEBUG:
     SECURE_REFERRER_POLICY = 'strict-origin-when-cross-origin'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
